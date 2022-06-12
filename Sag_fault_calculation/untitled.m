5a12e19bdac3cfaf9d40f3f308195e7f159311dc
Zayan <particlegriffin@gmail.com>
2022-06-12 14:59:19 +0800

    Changed code to suit linedata as table

--- Sag_fault_calculation/untitled.m
+++ Sag_fault_calculation/untitled.m
@@ -16,27 +16,13 @@ x = x.*100./s_old
 %% Y bus calculation
 
 % import data
-linedata = readmatrix('line_data.xlsx');
-impedances = linedata(:,3:5); % storing the impedances only
+linedata = readtable('line_data.xlsx');
+impedances = [linedata.R linedata.X linedata.B]; % storing the impedances only
 impedances(:,2) = impedances(:,2)*i;  % transforming into reactances
-nodes = linedata(:,1:2); % storing the buses/nodes only
+nodes = [linedata.FromNode linedata.ToNode]; % storing the buses/nodes only
 from_node = nodes(:,1);
 to_node = nodes(:,2);
 
 Y_bus = ones(14,14); % initializing the Y bus
-
-for index1=1:length(from_node) % from nodes
-    m = from_node(index1);
-    if from_node(index1)==m
-
-    end
-end
-
-% buses = 1:14;
-% for bus=1:14
-%     if bus~=from_node(bus)
-%         from_node = [from_node; bus];
-%     end
-% end
 
 % group linedata by from node
