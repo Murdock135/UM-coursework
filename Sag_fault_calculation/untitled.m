979372d0b3fbee7ed751ae559551757d12b44b37
Zayan <particlegriffin@gmail.com>
2022-06-13 08:29:16 +0800

    Update: Y bus has been created

--- Sag_fault_calculation/untitled.m
+++ Sag_fault_calculation/untitled.m
@@ -16,18 +16,17 @@ x = x.*100./s_old
 %% Y bus calculation
 
 % import data
-linedata = readtable('line_data.xlsx');
+linedata_table = readtable('line_data.xlsx');
 %% Setting up the data for processing
 
 % converting the node names into categorical type
-linedata.FromNode = string(linedata.FromNode);
-%linedata.ToNode = string(linedata.ToNode);
-linedata.FromNode = categorical(linedata.FromNode);
-linedata.ToNode = categorical(linedata.ToNode);
+linedata_table.FromNode = categorical(linedata_table.FromNode);
+linedata_table.ToNode = categorical(linedata_table.ToNode);
 
-impedances = [linedata.R linedata.X linedata.B]; % storing the impedances only
+impedances = [linedata_table.R linedata_table.X linedata_table.B]; % storing the impedances only
+
 impedances(:,2) = impedances(:,2)*i;  % transforming into reactances
-nodes = [linedata.FromNode linedata.ToNode]; % storing the buses/nodes only
+nodes = [linedata_table.FromNode linedata_table.ToNode]; % storing the buses/nodes only
 from_node = nodes(:,1);
 to_node = nodes(:,2);
 
@@ -38,16 +37,24 @@ unique_to_nodes = unique(to_node);
 %% calculating the diagonal elements
 % 
 y_diag = eye(14,14);
-for m=1:length(unique(from_node))
+for m=1:length(unique_from_nodes)
     z = impedances(from_node==unique_from_nodes(m),:);
     y_diag(unique_from_nodes(m), unique_from_nodes(m)) = 1/(sum(z(:,1))+sum(z(:,2))+sum(z(:,3)));
 end
 
 %% calculating off diagonal elements
 
+% preprocessing
+linedata_matrix = readmatrix("line_data.xlsx")
+nodes = linedata_matrix(:,1:2);
 y_off = zeros(14,14);
-for m=1:length(unique(from_node))
-    
-    z = impedances(from_node==unique_from_nodes(m),:);
-    y_diag(unique_from_nodes(m), unique_from_nodes(m)) = 1/(sum(z(:,1))+sum(z(:,2))+sum(z(:,3)));
+impedances = impedances(:,1)+impedances(:,2)+impedances(:,3);
+[r,c] = size(linedata_matrix);
+
+
+for m=1:r
+    y_off(nodes(m,1),nodes(m,2)) = -1/impedances(m);
 end
+
+Y_bus = y_off + y_diag
+
