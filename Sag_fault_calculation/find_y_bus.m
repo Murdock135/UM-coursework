6a17f0d63f5805bb5335a0117093cb0a66d1c8e6
Zayan <particlegriffin@gmail.com>
2022-06-13 14:36:37 +0800

    Fixed y bus matrix calculation

--- Sag_fault_calculation/find_y_bus.m
+++ Sag_fault_calculation/find_y_bus.m
@@ -9,6 +9,7 @@ function y_bus = find_y_bus(linedata)
     linedata_table.ToNode = categorical(linedata_table.ToNode);
     impedances = [linedata_table.R linedata_table.X linedata_table.B]; % storing the impedances only 
     impedances(:,2) = impedances(:,2)*i;  % transforming into reactances
+    impedances(:,3) = impedances(:,3)*i; % transforming into susceptances
     nodes = [linedata_table.FromNode linedata_table.ToNode]; % storing the buses/nodes only
     from_node = nodes(:,1);
     to_node = nodes(:,2);
@@ -16,10 +17,18 @@ function y_bus = find_y_bus(linedata)
     unique_to_nodes = unique(to_node);
     
     % calculating the diagonal elements 
-    y_diag = eye(14,14);
+    y_diag = zeros(14,14);
     for m=1:length(unique_from_nodes)
         z = impedances(from_node==unique_from_nodes(m),:);
-        y_diag(unique_from_nodes(m), unique_from_nodes(m)) = 1/(sum(z(:,1))+sum(z(:,2))+sum(z(:,3)));
+        [r,c] = size(z)
+        R = z(:,1)
+        X = z(:,2)
+        B = z(:,3)
+        for k=1:r
+            k
+            Y = -1/(R(k)+X(k))
+            y_diag(unique_from_nodes(m), unique_from_nodes(m)) = y_diag(unique_from_nodes(m), unique_from_nodes(m))+ Y+B(k);
+        end 
     end
     
     % calculating off diagonal elements
@@ -27,7 +36,7 @@ function y_bus = find_y_bus(linedata)
     linedata_matrix = readmatrix(linedata);
     nodes = linedata_matrix(:,1:2);
     y_off = zeros(14,14);
-    impedances = impedances(:,1)+impedances(:,2)+impedances(:,3);
+    impedances = impedances(:,1)+impedances(:,2);
     [r,c] = size(linedata_matrix);
     
     
@@ -35,4 +44,4 @@ function y_bus = find_y_bus(linedata)
         y_off(nodes(m,1),nodes(m,2)) = -1/impedances(m);
     end
 
-    y_bus = y_off + y_diag
+    y_bus = y_off + y_diag;
